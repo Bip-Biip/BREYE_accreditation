8be547a0469b1fc19e644a50cbdcaa88ca62ceaa
Ludwig <paul.chipot@hotmail.fr>
2021-12-16 14:35:56 +0100

    Modification du test des boutons avec notamment l'ajout du bouton Wi-Fi.

--- global_test.py
+++ global_test.py
@@ -20,63 +20,33 @@ from adafruit_pn532.i2c import PN532_I2C
 LEFT_PIN = 21
 MIDDLE_PIN = 20
 RIGHT_PIN = 16
+WIFI_PIN = 1
 
 # =================================================================================================
 #                                      Les boutons
 # =================================================================================================
 
-BUTTON_LEFT = 0
-BUTTON_MIDDLE = 1
-BUTTON_RIGHT = 2
-BUTTON_RELEASED = 0
-BUTTON_PRESSED = 1
-BUTTON_STAND_BY = 2
+flag = False
+waited_pin = -1
 
-buttons_state = [BUTTON_RELEASED]*3
+def button_callback(id) :
+    global flag
+    global waited_pin
+    if flag and id == waited_pin :
+        print("Bouton appuyé. pin :", id)
+        flag = False
+    return
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LEFT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(MIDDLE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(RIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-def setButtonState(button_id, button_state):
-
-	last_state = buttons_state[button_id]
-
-	if last_state == button_state:
-		return False
-
-	if last_state == BUTTON_STAND_BY and button_state == BUTTON_PRESSED:
-		return False
-
-	buttons_state[button_id] = button_state
-	
-	if last_state == BUTTON_STAND_BY and button_state == BUTTON_RELEASED:
-		return False
-
-	if button_state == BUTTON_RELEASED:
-		if sum([1 if buttons_state[b] == BUTTON_PRESSED else 0 for b in range(3)]) == 2:
-			for b in buttons_state :
-				if buttons_state[b] == BUTTON_PRESSED:
-					buttons_state[b] = BUTTON_STAND_BY
-			print("C'est un triple appui.")
-		elif sum([1 if buttons_state[b] == BUTTON_PRESSED else 0 for b in range(3)]) == 1:
-			double_id = [button_id]
-			for b in buttons_state :
-				if buttons_state[b] == BUTTON_PRESSED:
-					double_id.append(b)
-					buttons_state[b] = BUTTON_STAND_BY
-			print("C'est un double appui. [" + str(double_id[0]) + "] et [" + str(double_id[1]) + "]")
-		else :
-			if button_id == 0 :
-				print("Bouton gauche. [0]")
-			if button_id == 1 :
-				print("Bouton milieu. [1]")
-			if button_id == 2 :
-				print("Bouton droit. [2]")
-
-	return True
+GPIO.setup(WIFI_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(LEFT_PIN, GPIO.RISING, callback=button_callback)
+GPIO.add_event_detect(MIDDLE_PIN, GPIO.RISING, callback=button_callback)
+GPIO.add_event_detect(RIGHT_PIN, GPIO.RISING, callback=button_callback)
+GPIO.add_event_detect(WIFI_PIN, GPIO.RISING, callback=button_callback)
 
 # =================================================================================================
 #                                      Les hauts parleurs
@@ -111,29 +81,33 @@ pn532.SAM_configuration()
 # =================================================================================================
 #                                      Le script
 # =================================================================================================
+
 print("Bonjour et bienvenue dans le test Hardware de Br'Eye.")
 print("Nous allons tester ensemble si les périphériques sont fonctionnels.")
-print("Vous disposez de 10 secondes pour tester les boutons.")
-a = 0
-while a < 2000 :
-	# Left button
-	if GPIO.input(LEFT_PIN) != GPIO.HIGH :
-		x = setButtonState(BUTTON_LEFT, BUTTON_PRESSED)
-	else :
-		x = setButtonState(BUTTON_LEFT, BUTTON_RELEASED)
-	# Middle button
-	if GPIO.input(MIDDLE_PIN) != GPIO.HIGH :
-		y = setButtonState(BUTTON_MIDDLE, BUTTON_PRESSED)
-	else :
-		y = setButtonState(BUTTON_MIDDLE, BUTTON_RELEASED)
-	# Right button
-	if GPIO.input(RIGHT_PIN) != GPIO.HIGH :
-		z = setButtonState(BUTTON_RIGHT, BUTTON_PRESSED)
-	else :
-		z = setButtonState(BUTTON_RIGHT, BUTTON_RELEASED)
-	# Little pause
-	a += 1
-	time.sleep(0.005)
+
+flag = True
+waited_pin = MIDDLE_PIN
+print("Veuillez appuyer sur le bouton central.")
+while flag :
+    time.sleep(0.5)
+
+flag = True
+waited_pin = LEFT_PIN
+print("Veuillez appuyer sur le bouton gauche.")
+while flag :
+    time.sleep(0.5)
+
+flag = True
+waited_pin = RIGHT_PIN
+print("Veuillez appuyer sur le bouton droit.")
+while flag :
+    time.sleep(0.5)
+
+flag = True
+waited_pin = WIFI_PIN
+print("Veuillez appuyer sur le bouton wi-fi (caché).")
+while flag :
+    time.sleep(0.5)
 
 print("Bien, nous allons maintenant tester la détection des tag nfc.")
 print("Présentez un jeton de votre choix.")
