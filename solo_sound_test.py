c695e555543c7e04f430dd7cbe33fa91315db316
Bip-Biip <56770142+Bip-Biip@users.noreply.github.com>
2022-10-21 13:41:36 +0200

    Changement de modèle de carte son

--- solo_sound_test.py
+++ solo_sound_test.py
@@ -2,11 +2,12 @@ import time
 import board
 import busio
 import pygame
-import adafruit_max9744
+import adafruit_tpa2016
 
 i2c = busio.I2C(board.SCL, board.SDA)
-amp = adafruit_max9744.MAX9744(i2c)
-amp.volume = 32
+tpa = adafruit_tpa2016.TPA2016(i2c)
+tpa.fixed_gain = 0
+
 pygame.mixer.init()
 voice_channel = pygame.mixer.Channel(0)
 voice_sound = pygame.mixer.Sound('sound.wav')
@@ -14,10 +15,10 @@ voice_sound = pygame.mixer.Sound('sound.wav')
 print("Nous allons tester la carte son.")
 time.sleep(1)
 voice_channel.play(voice_sound)
-time.sleep(3)
+time.sleep(2)
 
 print("Attention les oreilles, nous allons maintenant augmenter le volume.")
-time.sleep(2)
-amp.volume = 50
+time.sleep(1)
+tpa.fixed_gain = 3
 voice_channel.play(voice_sound)
 time.sleep(3)
