1b8f13a22ffc89abc900658f6796b60160c73923
PaulC <paul.chipot@hotmail.fr>
2022-10-21 14:20:06 +0200

    test de volume

---
+++ solo_volume_test.py
@@ -0,0 +1,30 @@
+import time
+import board
+import busio
+import pygame
+import adafruit_tpa2016
+
+i2c = busio.I2C(board.SCL, board.SDA)
+tpa = adafruit_tpa2016.TPA2016(i2c)
+tpa.fixed_gain = -28
+growing = True
+
+pygame.mixer.init()
+voice_channel = pygame.mixer.Channel(0)
+voice_sound = pygame.mixer.Sound('music.mp3')
+voice_channel.play(voice_sound)
+
+for i in range(1200):
+    if growing:
+        if tpa.fixed_gain < 30:
+            tpa.fixed_gain += 1
+        else:
+            growing = False
+            tpa.fixed_gain -= 1
+    else:
+        if tpa.fixed_gain > -28:
+            tpa.fixed_gain -= 1
+        else:
+            growing = True
+            tpa.fixed_gain += 1
+        time.sleep(0.1)
