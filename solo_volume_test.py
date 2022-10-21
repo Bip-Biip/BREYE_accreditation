f433394625eae9340531146bbc199beb5b4eb30a
PaulC <paul.chipot@hotmail.fr>
2022-10-21 17:11:55 +0200

    Essai de provoquer ALSA lib avec plein de wav en simultané

--- solo_volume_test.py
+++ solo_volume_test.py
@@ -6,26 +6,13 @@ import adafruit_tpa2016
 
 i2c = busio.I2C(board.SCL, board.SDA)
 tpa = adafruit_tpa2016.TPA2016(i2c)
-tpa.fixed_gain = 0
-growing = True
+tpa.fixed_gain = -28
 
 pygame.mixer.init()
-voice_channel = pygame.mixer.Channel(0)
-voice_sound = pygame.mixer.Sound('music.mp3')
-voice_channel.play(voice_sound)
+voice_sound = pygame.mixer.Sound('music.wav')
 
-for i in range(30):
-    print(tpa.fixed_gain)
-    if growing:
-        if tpa.fixed_gain < 30:
-            tpa.fixed_gain += 1
-        else:
-            growing = False
-            tpa.fixed_gain -= 1
-    else:
-        if tpa.fixed_gain > 0:
-            tpa.fixed_gain -= 1
-        else:
-            growing = True
-            tpa.fixed_gain += 1
-    time.sleep(5)
+for i in range(60):
+    print(i+1)
+    voice_channel = pygame.mixer.Channel(i)
+    voice_channel.play(voice_sound)
+    time.sleep(1)
