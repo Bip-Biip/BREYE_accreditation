7ccd2038667251544657987003d5412ff6dceff5
PaulC <paul.chipot@hotmail.fr>
2022-10-24 09:43:17 +0200

    Test son de victoire

--- solo_volume_test.py
+++ solo_volume_test.py
@@ -6,14 +6,12 @@ import adafruit_tpa2016
 
 i2c = busio.I2C(board.SCL, board.SDA)
 tpa = adafruit_tpa2016.TPA2016(i2c)
-tpa.fixed_gain = -28
+tpa.fixed_gain = 6
 
 pygame.mixer.init()
-pygame.mixer.set_num_channels(60)
-voice_sound = pygame.mixer.Sound('music.wav')
-
-for i in range(60):
-    print(i+1)
-    voice_channel = pygame.mixer.Channel(i)
-    voice_channel.play(voice_sound)
-    time.sleep(1)
+# voice_sound = pygame.mixer.Sound('music.wav')
+# voice_sound = pygame.mixer.Sound('victoire.wav')
+voice_sound = pygame.mixer.Sound('victoire_au.wav')
+voice_channel = pygame.mixer.Channel(0)
+voice_channel.play(voice_sound)
+time.sleep(15)
